ff8b0a5c60fdc6bf6eea88dc20f9cee308354a45
Maciej Majewski <mmajewski724@gmail.com>
2025-10-19 13:17:47 +0200

    Revamped logic of queue

--- src/queue.rs
+++ src/queue.rs
@@ -1,10 +1,9 @@
-use serde::{Deserialize, Serialize};
-use tokio::{
-    fs,
-    io::{AsyncReadExt, AsyncWriteExt},
-};
+use std::path::PathBuf;
 
-use crate::utils::get_default_path;
+use serde::{Deserialize, Serialize};
+use tokio::{fs, io::AsyncReadExt, io::AsyncWriteExt};
+
+use crate::utils::get_playlists_dir;
 
 #[derive(Deserialize, Serialize, Clone)]
 pub struct Queue {
@@ -14,14 +13,11 @@ pub struct Queue {
 #[derive(Deserialize, Serialize, Clone)]
 pub struct QueueItem {
     pub file_path: String,
+    pub name: String,
 }
 impl Queue {
     pub async fn from_queue_json(playlist_name: &str) -> Result<Self, std::io::Error> {
-        let target_path = get_default_path()
-            .await?
-            .join("playlists")
-            .join(playlist_name)
-            .join("queue.json");
+        let target_path = get_queue_path(playlist_name).await?;
         let mut file = match fs::File::open(target_path).await {
             Ok(v) => v,
             Err(_) => {
@@ -32,17 +28,72 @@ impl Queue {
         };
         let mut buf = Vec::new();
         file.read_to_end(&mut buf).await?;
-        Ok(serde_json::from_slice(&buf).unwrap())
+        Ok(serde_json::from_slice(&buf)?)
     }
     pub async fn to_json(&self, playlist_name: &str) -> Result<(), std::io::Error> {
-        let default_path = get_default_path().await?;
-        let target_path = default_path
-            .join("playlists")
-            .join(playlist_name)
-            .join("queue.json");
+        let target_path = get_queue_path(playlist_name).await?;
         let mut file = fs::File::create(target_path).await?;
         let bytes = serde_json::to_vec_pretty(self)?;
         file.write(&bytes).await?;
         Ok(())
     }
+}
+
+pub async fn handle_renaming_audio(
+    playlist_name: &str,
+    new_name: String,
+    prev_name: &str,
+) -> Result<(), std::io::Error> {
+    let target_path = get_queue_path(playlist_name).await?;
+    let mut file = fs::File::open(&target_path).await?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).await?;
+    let mut serialized: Queue = serde_json::from_slice(&buf)?;
+    let serialized_items = &mut serialized.items;
+    if let Some(pos) = serialized_items
+        .iter_mut()
+        .position(|x| x.name == prev_name)
+    {
+        serialized_items[pos].name = new_name;
+    }
+    let mut file = fs::File::create(target_path).await?;
+    file.write_all(&serde_json::to_vec_pretty(&serialized)?)
+        .await?;
+    Ok(())
+}
+pub async fn handle_removing_audio(
+    audio_name: &str,
+    playlist_name: &str,
+) -> Result<(), std::io::Error> {
+    let target_path = get_queue_path(playlist_name).await?;
+    let mut file = fs::File::open(&target_path).await?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).await?;
+    let mut serialized: Queue = serde_json::from_slice(&buf)?;
+    let serialized_items = &mut serialized.items;
+    if let Some(pos) = serialized_items
+        .iter_mut()
+        .position(|x| x.name == audio_name)
+    {
+        serialized_items.remove(pos);
+    }
+    let mut file = fs::File::create(target_path).await?;
+    file.write_all(&serde_json::to_vec_pretty(&serialized)?)
+        .await?;
+    Ok(())
+}
+pub async fn handle_getting_queue(playlist_name: &str) -> Result<Vec<String>, std::io::Error> {
+    let target_path = get_queue_path(playlist_name).await?;
+    let mut file = fs::File::open(&target_path).await?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).await?;
+    let serialized: Queue = serde_json::from_slice(&buf)?;
+    let audio_names = serialized.items.into_iter().map(|v| v.name).collect();
+    Ok(audio_names)
+}
+async fn get_queue_path(playlist_name: &str) -> Result<PathBuf, std::io::Error> {
+    Ok(get_playlists_dir()
+        .await?
+        .join(playlist_name)
+        .join("queue.json"))
 }
